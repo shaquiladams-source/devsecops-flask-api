ab7e3c398057c6625a8724992dbb7826a6c03e78
Shaquil Adams <shaquiladams@Shaquils-MacBook-Pro.local>
2025-11-18 13:38:15 -0500

    re-test: final push after OIDC fix

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def health():
 
 @app.route("/version")
 def version():
-    # CI/CD will eventually  inject this env var
+    # CI/CD will eventually inject this env var
     git_sha = os.getenv("GIT_SHA", "local-dev")
     return jsonify({"version": git_sha}), 200
 
